ff9093a6d4407ddb771b3c2a17fa2a230c7cd2ec
Ujjwal <your_email@gmail.com>
2026-06-07 19:35:09 +0530

    Data ingestion

--- setup.py
+++ setup.py
@@ -4,6 +4,9 @@ from typing import List
 HYPEN_E_DOT = '-e .'
 
 def get_requirements(file_path: str) -> List[str]:
+    '''
+    This function will return the list of requirements
+    '''
 
     requirements = []
 
