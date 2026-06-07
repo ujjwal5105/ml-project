6807c82b8178c2e9be4a57daf3d12df887ee8777
Ujjwal <your_email@gmail.com>
2026-06-07 15:27:42 +0530

    EDA and Promlem statement

--- src/exception.py
+++ src/exception.py
@@ -1,3 +1,14 @@
 import sys 
 
-df error_massage_detail(error,error_detail:sys):
+def error_massage_detail(error,error_detail:sys):
+    _,_,exc_tb = error_detail.exc_info()
+    FileExistsError = exc_tb.tb_frame.f_code.co_filename
+    errror_massage = "Error occured in python script name [{0}] line number [{1}] error message [{2}]".format(
+        FileExistsError, exc_tb.tb_lineno, str(error)
+    )
+    class CustomException(Exception):
+        def __init__(self,error_message , error_detail:sys):
+            super().__init__(error_message)
+            self.error_message = error_massage_detail(error_message,detail=error_detail)
+        def __str__(self):
+            return self.error_message 
