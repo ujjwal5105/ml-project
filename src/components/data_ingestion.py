ae685b53d73e65bb62197516910e1a3bd9df1dce
Ujjwal <your_email@gmail.com>
2026-06-10 19:24:26 +0530

    Model trainer

--- src/components/data_ingestion.py
+++ src/components/data_ingestion.py
@@ -10,6 +10,9 @@ from dataclasses import dataclass
 from src.components.data_transformation import DataTransformation
 from src.components.data_transformation import DataTransformationConfig
 
+from src.components.model_trainer import ModelTrainerConfig
+from src.components.model_trainer import ModelTrainer
+
 print("Data Ingestion component")
 @dataclass
 class DataIngestionConfig:
@@ -45,7 +48,14 @@ if __name__ == "__main__":
             train_data, test_data = obj.initiate_data_ingestion()
 
             data_transformation = DataTransformation()
-            data_transformation.initiate_data_transformation(train_data, test_data)
+            train_arr, test_arr, = data_transformation.initiate_data_transformation(train_data, test_data)
+
+            model_trainer = ModelTrainer()
+            model_trainer.initiate_model_trainer(train_arr, test_arr)
+            
+            modeltrainer=ModelTrainer()
+            print(modeltrainer.initiate_model_trainer(train_arr, test_arr)) 
+
 
                
            
--- src/utils.py
+++ src/utils.py
@@ -1,8 +1,11 @@
 import os 
 import sys
+
+from sklearn.metrics import r2_score
 import dill
 
-import pandas as np
+
+import numpy as np
 import pandas as pd
 
 from src.exception import CustomException
@@ -17,3 +20,26 @@ def save_object(file_path, obj):
 
     except Exception as e:
          raise CustomException(e, sys)
+    
+def evaluate_models(X_train, y_train, X_test, y_test, models):
+    try:
+       
+        report = {}
+
+        for i in range(len(list(models))):
+            model = list(models.values())[i]
+            model.fit(X_train, y_train)
+
+            y_test_pred = model.predict(X_test)
+            y_train_pred = model.predict(X_train)
+
+            test_model_score = r2_score(y_train, y_train_pred)
+            test_model_score = r2_score(y_test, y_test_pred)
+
+            report[list(models.keys())[i]] = test_model_score
+
+        return report
+
+    except Exception as e:
+         raise CustomException(e, sys) 
+    
