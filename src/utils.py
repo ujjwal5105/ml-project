5d77dfc5e540d7deb2a090a5bba7bc99ed953881
Ujjwal <your_email@gmail.com>
2026-06-11 03:31:42 +0530

    Hyperparamater trianing

--- src/utils.py
+++ src/utils.py
@@ -4,6 +4,7 @@ import sys
 from sklearn.metrics import r2_score
 import dill
 
+from sklearn.model_selection import GridSearchCV
 
 import numpy as np
 import pandas as pd
@@ -21,14 +22,22 @@ def save_object(file_path, obj):
     except Exception as e:
          raise CustomException(e, sys)
     
-def evaluate_models(X_train, y_train, X_test, y_test, models):
+def evaluate_models(X_train, y_train, X_test, y_test, models, param):
     try:
        
         report = {}
 
         for i in range(len(list(models))):
             model = list(models.values())[i]
+            para = param.get(list(models.keys())[i])
+
+            gs = GridSearchCV(model,para,cv=3)
+            gs.fit(X_train, y_train)
+
+            model.set_params(**gs.best_params_)
             model.fit(X_train, y_train)
+          
+           # model.fit(X_train, y_train)
 
             y_test_pred = model.predict(X_test)
             y_train_pred = model.predict(X_train)
